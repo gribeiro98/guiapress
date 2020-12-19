f27a28b9752fb211d67ef71d7bfffe4ae4b9ede3
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2020-12-19 20:58:38 -0300

    Configuração de conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Imports
 const express = require('express');
 const app = express();
 const port = 8080;
 const bodyParser = require('body-parser');
+const connection = require('./database/database');
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -12,6 +14,14 @@ app.use(express.static('public'));
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Database Connection
+connection.authenticate()
+    .then(() => {
+        console.log('Conexão com banco de dados realizada com sucesso');
+    }).catch((error) => {
+        console.log('Erro ao se conectar com o banco de dados', error.message);
+    });
 
 app.get('/', (req, res) => {
     res.render('index');
