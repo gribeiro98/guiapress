4ea2bb1b574b41ea68a8e4940e5c9dfafc8b2cda
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2020-12-19 20:45:55 -0300

    Configurado EJS como View Engine no express

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// View Engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.send('Bem vindo!');
 });
