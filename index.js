9ccd1a6eab8e2d0de65e0fd3e4754df3047ec002
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2020-12-19 20:48:17 -0300

    Configurado BodyParser

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser');
 
 // View Engine
 app.set('view engine', 'ejs');
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.render('index');
