ee2a70d99797e11375898cc4664b5d87a523c327
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2020-12-21 20:39:21 -0300

    Removido método sync dos modelos para não ficar gerando log no console

--- articles/Article.js
+++ articles/Article.js
@@ -20,6 +20,6 @@ const Article = connection.define('articles', {
 Category.hasMany(Article);
 Article.belongsTo(Category);
 
-Article.sync({ force: false });
+// Article.sync({ force: false });
 
 module.exports = Article;
--- categories/Category.js
+++ categories/Category.js
@@ -12,6 +12,6 @@ const Category = connection.define('categories', {
     }
 });
 
-Category.sync({ force: false });
+// Category.sync({ force: false });
 
 module.exports = Category;
