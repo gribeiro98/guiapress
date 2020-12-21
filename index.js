ec4a11895d7a076db019ff39b9fdc83e81ebe5f4
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2020-12-21 20:34:19 -0300

    require dos modelos criados no arquivo index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const connection = require('./database/database');
 const categoriesController = require('./categories/CategoriesController');
 const articlesController = require('./articles/ArticlesController');
 
+// Models
+const Category = require('./categories/Category');
+const Article = require('./articles/Article');
+
 // View Engine
 app.set('view engine', 'ejs');
 
