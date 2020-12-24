66f955e6c7427b017a43c4a72afada765f346f37
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2020-12-24 11:46:07 -0300

    Ajustado busca dos parâmetros de conexão do arquivo .env

--- database/database.js
+++ database/database.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const connection = new Sequelize('guiapress', 'root', '', {
-    host: 'localhost',
+console.log(process.env.APP_NAME);
+
+const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     timezone: '-03:00'
 });
