8c17f8d6272dc714ed1bfee57763d4d129304362
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2020-12-25 21:58:20 -0300

    Criado processo de edição de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -54,8 +54,50 @@ router.post('/articles/delete', (req, res) => {
     } else {
         res.redirect('/admin/articles');
     }
+});
 
+router.get('/admin/articles/edit/:id', (req, res) => {
+   var id = req.params.id;
+   
+   if(id != undefined) {
+       if(!isNaN(id)) {
+            Article.findByPk(id).then(article => {
+                Category.findAll().then(categories => {
+                    res.render('admin/articles/edit', {article: article, categories: categories});
+                });
+            }).catch(err => {
+                res.redirect('/admin/articles');
+            });
+       } else {
+           res.redirect('/admin/articles');
+       }
+   } else {
+       res.redirect('/admin/articles');
+   }
+});
 
+router.post('/articles/update', (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({
+        title: title,
+        slug: slugify(title, { lower: true }),
+        body: body,
+        categoryId: category
+    }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect('/admin/articles');
+    }).catch(err => {
+        res.redirect('/admin/articles');
+    });
+    
+ 
 });
 
 module.exports = router;
