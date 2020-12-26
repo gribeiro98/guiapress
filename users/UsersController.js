1f58a11835468a47a22e082f9242f761f35098c4
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2020-12-26 20:46:36 -0300

    Criado processo de cadastro de usuários

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
     res.send('Listagem de usuários');
@@ -8,6 +9,33 @@ router.get('/admin/users', (req, res) => {
 
 router.get('/admin/users/new', (req, res) => {
     res.render('admin/users/new');
+});
+
+router.post('/users/save', (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({
+        where: {
+            email: email
+        }
+    }).then(user => {
+        if(user == undefined) {
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+            
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect('/admin/users');
+            }).catch((err) => {
+                res.redirect('/admin/users');
+            });
+        } else {
+            res.redirect('/admin/users/new')
+        }
+    });
 });
 
 module.exports = router;
